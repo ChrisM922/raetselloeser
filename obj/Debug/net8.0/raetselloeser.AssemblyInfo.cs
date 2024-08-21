8b35ba48764a585a69f10159fc89f4ae40e2181e
ChrisM922 <112943995+ChrisM922@users.noreply.github.com>
2024-08-21 13:26:06 +0200

    Add initial project files and dependencies

--- obj/Debug/net8.0/raetselloeser.AssemblyInfo.cs
+++ obj/Debug/net8.0/raetselloeser.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("raetselloeser")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8e800d0e9d18b06607389dedea78395c9484437")]
 [assembly: System.Reflection.AssemblyProductAttribute("raetselloeser")]
 [assembly: System.Reflection.AssemblyTitleAttribute("raetselloeser")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
